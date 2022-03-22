937e7b50dc2901ff6bb132c1b208e8538f0abd3b
dev amila <vdalakmal@gmail.com>
2022-03-22 11:39:10 +0530

    test components added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+function Greeting() {
+  return <section>
+    <Book></Book>
+  </section>;
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Book = () => {
+  return  <article>
+    <Image></Image>
+    <Title></Title>
+    <Author></Author>
+    <Input></Input>
+    <App></App>
+
+  </article>
+}
+
+const Image = () => {
+  return <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSlWR55VTSq5rQ5bjdm9bjNtT75d0KZcd8Lv1Jn72v-GGxQPJaa"></img>
+}
+const Title = () => <h1> Book Title</h1>
+const Author = () => <h4> Author</h4>
+const Input = () => <input type="text" onChange={()=>console.log('hi')}/>
+
+class App extends Component {
+  state = {
+    name: ''
+  };
+  render() {
+    return (
+      <div>
+        <p>Updated name: {this.state.name}</p>
+        <p>Change name:</p>
+        <input
+          type="text"
+          onChange={e => {
+            this.setState({
+              name: e.target.value
+            });
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+
+
+ReactDOM.render(<Greeting />, document.getElementById("root"));
